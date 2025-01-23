a7ee008670ab48bede02834db22c6d2bda83489c
Kim Hemmerling <58479760+khemmerling@users.noreply.github.com>
2025-01-23 10:36:21 -0500

    minor edits

--- parallelAnalysis.m
+++ parallelAnalysis.m
@@ -1,4 +1,4 @@
-function [numPCs, not_converged, score, sd_score, latent, sd_latent] = parallelAnalysis(prefix, basedir, datafolder, resultsfolder, slices, spv, perm, maxiter)
+function [numPCs, not_converged, score, sd_score, latent, sd_latent] = parallelAnalysis(prefix, basedir, datafolder, resultsfolder, slices, spv, perm, maxiter, truncate)
 %PARALLELANALYSIS compares IAAFT generated surrogates to fMRI timeseries
 %data to determine a number of principal components in a region outside the
 %spinal cord that explain more variance than the similar surrogate timeseries
@@ -12,6 +12,7 @@ function [numPCs, not_converged, score, sd_score, latent, sd_latent] = parallelA
 %spv: number of surrogates to generate per voxel (value)
 %perm: number of surrogate slices to generate by permutation (value)
 %maxiter: max number iterations for IAAFT (value)
+%truncate: number of timepoints (TRs) to truncate from end of timeseries
 
 %OUTPUTS
 %numPCs: number of PCs decided for each slice (vector)
@@ -24,11 +25,23 @@ function [numPCs, not_converged, score, sd_score, latent, sd_latent] = parallelA
 %load timeseries
 fprintf('\nLoading timeseries data.')
 timeseries_ns = load([basedir '/' datafolder '/' prefix '_notSpine_timeseries.txt']);
+if truncate>0
+    % truncate TRs if requested from the END
+    % (e.g., useful for data with different number of timepoints)
+    timeseries_ns=timeseries_ns(1:end-truncate,:);
+    fprintf('Truncated %d timepoints from END. New size: %d TRs.\n',truncate,size(timeseries_ns,1)-3)
+elseif truncate<0
+    % truncate TRs if requested from the START (e.g., for steady state)
+    truncate=-truncate;
+    timeseries_ns=[timeseries_ns(1:3,:); timeseries_ns(4+truncate:end,:)];
+    fprintf('Truncated %d timepoints from START. New size: %d TRs.\n',truncate,size(timeseries_ns,1)-3)
+end
 %predefine variables
 not_converged = 0; num_slices = length(slices);
 numPCs = zeros(num_slices, 1); [rows, cols] = size(timeseries_ns);
 score = cell(num_slices, 1); sd_score = cell(num_slices, perm);
 latent = zeros((rows-4), num_slices); sd_latent = cell(num_slices, 1);
+sd_latent_mean = zeros((rows-4), num_slices); 
 
 %generate surrogates for each voxel
 for s = slices
@@ -64,16 +77,23 @@ for s = slices
     %perform pca on template and compare eigenvalues to determine numPCs
     [~, score{s+1, 1}, l] = pca(slice_ts);
     numPCs(s+1, 1) = sum(l>=mean(sd_l, 2));
-    latent(:, s+1) = l; sd_latent{s+1, 1} = sd_l;
+    latent(:, s+1) = l; 
+    sd_latent{s+1, 1} = sd_l;
+    sd_latent_mean(:, s+1) = mean(sd_l, 2);
     
-%     %uncomment below section to include scree plot
-%     figure(); plot([mean(sd_l, 2), l]); xline(numPCs(s+1, 1));
+    % Uncomment below to save scree plot
+%     %make scree plot (save without opening)
+%     f=figure('visible','off'); 
+%     plot([mean(sd_l, 2), l]); xline(numPCs(s+1, 1));
 %     xlabel("Number of PCs"); ylabel("Eigenvalues"); title([prefix ' Slice ' num2str(s) ' Scree Plot '], 'Interpreter', 'none');
 %     legend("Average Surrogate Eigenvalues", "Template Eigenvalues", "PC Cutoff");
+%     saveas(f,[resultsfolder '/' prefix '_scree_slice' num2str(s) '.svg']);
 end
 fprintf('\n');
 
 %save results
-save([basedir '/' resultsfolder '/' prefix '_inputs.mat'], 'maxiter', 'perm', 'spv');
-save([basedir '/' resultsfolder '/' prefix '_results.mat'], 'latent', 'not_converged', 'numPCs', 'score', 'sd_latent', 'sd_score');
+save([resultsfolder '/' prefix '_inputs.mat'], 'maxiter', 'perm', 'spv');
+save([resultsfolder '/' prefix '_results.mat'], 'latent', 'not_converged', 'numPCs', 'score', 'sd_latent_mean');
+% currently not saving sd_latent or sd_score because they are large and not
+% necessary 
 end
